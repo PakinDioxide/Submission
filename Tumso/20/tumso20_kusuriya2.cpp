0b6aa3518d52cf14b0d1c859b868fcc674c6470f
Pakin Olanraktham <125773754+PakinDioxide@users.noreply.github.com>
2024-02-06 22:44:07 +0700

    Update tumso20_kusuriya2.cpp

--- Tumso/20/tumso20_kusuriya2.cpp
+++ Tumso/20/tumso20_kusuriya2.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 long long ans = LLONG_MAX, n;
-vector <int> s, b;
+vector <int> a, b;
 
-void sub(int i, long long ss, long long bb, int alr) {
+void sub(int i, long long aa, long long bb, int alr) {
     if (i == n) {
         if (!alr) return;
-        long long h = abs(ss-bb);
+        long long h = abs(aa-bb);
         long long l = 0;
         for (int j = 1; j <= h; j++) l += j*j*j - j*j + j;
         ans = min(ans, l);
         return;
     }
 
-    sub(i+1, ss, bb, alr == 1 ? 1 : 0);
-    sub(i+1, ss+s[i], bb*b[i], 1);
+    sub(i+1, aa, bb, alr == 1 ? 1 : 0);
+    sub(i+1, aa+a[i], bb*b[i], 1);
 }
 
 int main() {
     cin >> n;
-    s.resize(n);
+    a.resize(n);
     b.resize(n);
 
-    for (int i = 0; i < n; i++) cin >> b[i] >> s[i];
+    for (int i = 0; i < n; i++) cin >> b[i] >> a[i];
 
     sub(0, 0, 1, 0);
 
