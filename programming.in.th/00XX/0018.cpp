78b1658dc129ea362cb949adc01c699e7314b3ca
Pakin Olanraktham <125773754+PakinDioxide@users.noreply.github.com>
2024-02-11 15:21:33 +0700

    Update 0018.cpp

--- programming.in.th/00XX/0018.cpp
+++ programming.in.th/00XX/0018.cpp
@@ -5,20 +5,23 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    vector <int> alr(n);
+    vector<int> alr(n+1);
 
     int c = 0, j = 2;
     while (c != k) {
-        for (int i = j; i <= n; i+=j) {
-            if (!alr[i-1]) {
-                c++;
-                alr[i-1] = 1;
-                if (c == k) {
-                    cout << i;
-                    return 0;
+        if (!alr[j]) {
+            for (int i = j; i <= n; i += j) {
+                if (!alr[i]) {
+                    c++;
+                    alr[i] = 1;
+                    if (c == k) {
+                        cout << i;
+                        return 0;
+                    }
                 }
             }
         }
         j++;
     }
+    return 0;
 }
