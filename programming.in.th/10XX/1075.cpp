74da7ce2eca69113d6a5e866a7a7998db6602a3e
Pakin Olanraktham <125773754+PakinDioxide@users.noreply.github.com>
2024-09-12 12:44:59 +0700

    Update 1075.cpp

--- programming.in.th/10XX/1075.cpp
+++ programming.in.th/10XX/1075.cpp
@@ -16,7 +16,7 @@ int32_t main() {
     c[0][0] = c[0][1] = 0;
     for (int i = 1; i <= n; i++) {
         dp[i][0] = c[i-1][0] >= c[i-1][1] ? dp[i-1][0] : dp[i-1][1];
-        dp[i][1] = (float) (dp[i][0].first + a[i].second) / max(dp[i][0].second, a[i].first) >= (float) (dp[i][1].first + a[i].second) / max(dp[i][1].second, a[i].first) ? make_pair(dp[i][0].first + a[i].second, max(dp[i][0].second, a[i].first)) : make_pair(dp[i][1].first + a[i].second, max(dp[i][1].second, a[i].first));
+        dp[i][1] = make_pair(dp[i][0].first + a[i].second, max(dp[i][0].second, a[i].first));
         c[i][0] = (float) dp[i][0].first / dp[i][0].second;
         c[i][1] = (float) dp[i][1].first / dp[i][1].second;
     }
